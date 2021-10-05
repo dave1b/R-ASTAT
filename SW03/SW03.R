1123edb546808e013df24b584047255351bcbebe
dave1b <57140925+dave1b@users.noreply.github.com>
2021-10-05 18:28:50 +0200

    done with exercises of SW03

--- SW03/SW03.R
+++ SW03/SW03.R
@@ -1,5 +1,4 @@
 # Aufgabe 3.1
-
 setwd("C:/Users/Dave/Documents/GitHub/R-Code Dave/SW03")
 geysir <- read.table("geysir.dat", header = TRUE)
 
@@ -9,14 +8,12 @@ hist(geysir[, "Zeitspanne"], xlab = "Zeitspanne zwischen 2 Erruptionen", breaks 
 hist(geysir[, "Zeitspanne"], xlab = "Zeitspanne zwischen 2 Erruptionen", breaks = seq(41, 96, by = 11))
 hist(geysir[, "Eruptionsdauer"])
 
-
 # Aufgabe 3.2
 mannfrau <- read.table("../SW02/mannfrau.csv", header = TRUE, sep = ",")
 mannfrau
 groesse.mann = mannfrau[, "groesse.mann"]
 groesse.frau = mannfrau[,"groesse.frau"]
 groesse.mann
-
 
 plot(groesse.mann,groesse.frau,
      xlab = "Grösse der Männer",
@@ -26,11 +23,6 @@ plot(groesse.mann,groesse.frau,
 
 abline(lm(groesse.frau ~ groesse.mann), col = "deepskyblue")
 
-
-
-
-
-
 # Aufgabe 3.3
 study <- read.table("income.dat", header = TRUE)
 head(study)
@@ -39,32 +31,49 @@ income
 schoolYears <- study[,"Educ"]
 IQ <- study[,"AFQT"]
 
-
-plot(schoolYears, income,      
-     xlab = "school years",
+plot(schoolYears, income,
+     main = "Streudiagramm von School Years und Income",
+     xlab = "School Years",
      ylab = "income",
      col = "blue",
-     pch = 20)
-
+     pch = 20,
+     ylim = c(0, 200000)
+     )
 abline(lm(income ~ schoolYears), col = "deepskyblue")
 
 plot(IQ, income,
+     main = "Streudiagramm von IQ und Income",
      xlab = "IQ",
-     ylab = "incone",
+     ylab = "Incone",
      col = "green",
-     pch = 20
+     pch = 20,
+     ylim = c(0, 200000)
      )
-
 abline(lm(income ~ IQ), col = "deepskyblue")
 
 
 
+# Aufgabe 3.4
+head(anscombe)
+
+plot(anscombe$x1, anscombe$y1)
+reg <- lm(anscombe$y1 ~ anscombe$x1)
+abline(reg)
+
+plot(anscombe$x2, anscombe$y2)
+reg <- lm(anscombe$y2 ~ anscombe$x2)
+abline(reg)
+
+plot(anscombe$x3, anscombe$y3)
+reg <- lm(anscombe$y3 ~ anscombe$x3)
+abline(reg)
+
+plot(anscombe$x4, anscombe$y4)
+reg <- lm(anscombe$y4 ~ anscombe$x4)
+abline(reg)
 
 
-
-
-
-
+# legacy
 waageA <- c(79.98,
             80.04,
             80.02,
@@ -78,7 +87,6 @@ waageA <- c(79.98,
             80.02,
             80.00,
             80.02)
-
 hist(
   waageA,
   freq = F,
