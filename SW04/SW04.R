4168f7ab7b0cfc9575d7e3db320515237887b6db
dave1b <57140925+dave1b@users.noreply.github.com>
2021-10-12 22:24:32 +0200

    finished exercises of SW04

--- SW04/SW04.R
+++ SW04/SW04.R
@@ -7,5 +7,28 @@ t.y
 t.y1 <- t.x1^2
 t.y1
 
-
+par(mfrow = c(1, 2))
 plot(t.y)
+plot(t.y1)
+cor(t.x, t.y)
+cor(t.x1, t.y1)
+
+
+#Aufgabe 4.5
+A <- 3/4
+B <- 2/3
+library(MASS)
+#a
+fractions(A*B)
+#b
+fractions(A+B - A*B)
+#c
+1-fractions(A*B)
+#d
+1-fractions(A+B - A*B)
+
+#4.6
+erdbeben <- 0.04
+taifung <- 0.08
+
+erdbeben+taifung - erdbeben*taifung
