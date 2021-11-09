2ec5bc96dcc7c710d9acb23c1c54c740f3926b37
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2021-11-09 08:31:46 +0100

    finished exercises of SW07

--- SW07/SW07.R
+++ SW07/SW07.R
@@ -3,3 +3,17 @@
 1-pnorm(q=185, mean =180, sd=7.4)
 #a.2
 pnorm(q= 180, mean =180, sd=7.4)-pnorm(q= 170, mean =180, sd=7.4)
+#b
+qnorm(p= 0.25, mean =180, sd=7.4)
+qnorm(p= 0.75, mean =180, sd=7.4)
+#c
+qnorm(p= 0.95, mean =180, sd=7.4)
+
+#Aufgabe 7.2
+#a
+pnorm(2.5,4,1.25)
+1-pnorm(5,4,1.25)
+#b
+100*(pnorm(4.5,4,1.25)-pnorm(3,4,1.25))
+#c
+qnorm(0.98,4,1.25)
