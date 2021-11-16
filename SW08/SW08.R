105b4c5219fec1792938c85b4f224527be606db5
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2021-11-16 08:30:37 +0100

    solved exercises of SW08

---
+++ SW08/SW08.R
@@ -0,0 +1,16 @@
+#Aufgabe 81
+#a
+pnorm(10,8.2,6^2/36)
+#b
+pnorm(10,8.2,6^2/36)-pnorm(5,8.2,6^2/36)
+#c
+1-pnorm(20,8.2,6^2/36)
+
+#Aufgabe 8.2
+1-pnorm(3.1,2.2,0.3^2/100)
+
+#Aufgabe3 8.3
+#a
+pnorm(82,77,sqrt(15^2/25))-pnorm(72,77,sqrt(15^2/25))
+#b
+pnorm(82,77,sqrt(15^2/64))-pnorm(72,77,sqrt(15^2/64))
