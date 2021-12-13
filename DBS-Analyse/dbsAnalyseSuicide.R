8b0357c737f15fef67b503dcead9a4f54ca6ed9b
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2021-12-13 19:04:59 +0100

    analysing multiple linear regression

--- DBS-Analyse/dbsAnalyseSuicide.R
+++ DBS-Analyse/dbsAnalyseSuicide.R
@@ -1,14 +1,14 @@
 setwd("C:/Users/Dave/Documents/GitHub/R-Code Dave/DBS-Analyse")
-datenSet <- read.csv("AllData_africa_2019.csv")#[, -1]
-datenSet
+datenSet <- read.csv("AllData_Europe_2015.csv")#[, -1]
+#datenSet <-  na.omit(datenSet) 
 #Economic
 pairs(~suicideRateTotal+unemploymentPTotal+RatioAtNationalPovertyLine+MeanPPP_Month+GiniIndex+GrossNationalIncome,
       data = datenSet, pch = "+", col = "black")
 
-#HealtExpendature
+#HealthExpendature
 pairs(~suicideRateTotal+CHE_GDP_Percentage+OOPE_CHE_Percentage+PVT_CHE_Percentage+GGHE_CHE_Percentage+CHI_CHE_Percentage,
       data = datenSet, pch = "+", col = "black")
-#Consumption
+#Consumption 
 pairs(~suicideRateTotal+TotalAlcoholConsumption,
       data = datenSet, pch = "+", col = "black")
 #HealthWorker
@@ -20,3 +20,28 @@ pairs(~suicideRateTotal+GiniIndex,
 
 summary(datenSet)
 
+
+# -------------------------------------
+
+library(ggplot2)
+library(dplyr)
+library(broom)
+library(ggpubr)
+
+scatter.smooth(x=datenSet$suicideRateTotal, y=datenSet$CHE_GDP_Percentage, main="Dist ~ Speed")  
+data_omit <- na.omit(datenSet)    
+cor(data_omit$suicideRateTotal, data_omit$GiniIndex) 
+
+
+
+income.graph<-ggplot(datenSet, aes(x=suicideRateTotal, y=CHE_GDP_Percentage))+
+   geom_point()
+income.graph <- income.graph + geom_smooth(method="lm", col="black")
+income.graph <- income.graph + stat_regline_equation(label.x = 3, label.y = 7)
+income.graph
+
+
+
+
+reg <- lm(datenSet$suicideRateTotal ~ datenSet$GiniIndex + datenSet$MedianPPP_Month + datenSet$HapinessScore)
+summary(reg)
